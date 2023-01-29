655f886f4a4871ebaa11593bcc03425e54f04a23
Noor Mohammad <97360324+NoorMohammadPatwary@users.noreply.github.com>
2023-01-29 13:31:08 +0600

    Update About.js

--- react_ecommerce/src/Components/About.js
+++ react_ecommerce/src/Components/About.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>About</div>
+    <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur repudiandae ea voluptatum, rerum dolorem dolores tempora dicta deleniti aliquam obcaecati tempore placeat laborum consequatur. Molestias nemo tenetur ab ullam nesciunt.</div>
   )
 }
 
